53e6daac5911fab0e19d50c1adc911282e7d6826
chomookun <chomookun@gmail.com>
2024-11-28 20:45:55 +0900

    .

--- example/vue-example/src/main.js
+++ example/vue-example/src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue';
+import Home from './views/HomeIndex.vue';
 import MasterDetail from './views/MasterDetail.vue';
 import HubAndSpoke from './views/HubAndSpoke.vue';
 
 const routes = [
-    { path: "/", name: "Home", component: Home},
+    { path: "/", name: "HomeIndex", component: Home},
     { path: "/master-detail", name: "MasterDetail", component: MasterDetail},
     { path: "/hub-and-spoke", name: "HubAndSpoke", component: HubAndSpoke}
 ];
